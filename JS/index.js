0438b7e56e8281ddbfddf64745f11be5a663ec1b
nayar <guermoucherayanyacine@gmail.com>
2023-04-18 16:14:54 +0200

    quatriéme commit

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,4 @@
-fetch('http://localhost:3000/api/products') 
-
+fetch('http://localhost:3000/api/products')
 
     .then(response => response.json())
     
--- JS/prooduct.js
+++ JS/prooduct.js
@@ -34,6 +34,8 @@ async function displayProduct(){
 };
 displayProduct()
 
+
+
 function getValue(){
 
     var button = document.getElementById("addToCart").value;
@@ -47,14 +49,41 @@ function getValue(){
   const arrayKanap = [id, color, quantity, price];
   console.table(arrayKanap);
 
+  // //   arrayKanap = localStorage.getItem("arrayKanap");
+
+//   let arrayKanapLinea = JSON.stringify(arrayKanap);
+//   window.localStorage.setItem("arrayKanapLinea", arrayKanap);
+// console.log(localStorage);
+
 
 //   arrayKanap = localStorage.getItem("arrayKanap");
 //   if(arraykanap === null){
 //   }else{
+//     let cardKanapLinea = JSON.stringify(cardKanap);
+//     window.localStorage.setItem("arrayCartLinea", cardKanap);
 //   }
 
+//   let arKanapLinea = JSON.stringify(arrayKanap); 
+
+//   let getKanap = localStorage.setItem("arrayKanapLinea", arrayKanap);
+
+
+  let arKanap = localStorage.getItem("arKanap");
+if (arKanap === null) {
+    let getKanap = [id, quantitys, color, prices];
+    let jsonKanap = JSON.stringify(getKanap);
+    window.localStorage.setItem ("arKanap", jsonKanap);
+}
+else {
+    let parseKanap = JSON.parse(arKanap);
+    parseKanap.push(id, quantitys, color, prices);
+    let jsonParseKanap = JSON.stringify(parseKanap);
+    localStorage.setItem ("arKanap", jsonParseKanap);
+}
 console.log(localStorage);
+// localStorage.clear();
 
-
-};
+}
 getValue();
+
+
